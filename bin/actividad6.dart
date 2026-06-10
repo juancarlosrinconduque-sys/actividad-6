11536f4937352bdadacff0c32b13e7b450914e4c
juan <juancarlosrinconduque@gmail.com>
2026-06-10 09:32:46 -0500

    segunda version

--- bin/actividad6.dart
+++ bin/actividad6.dart
@@ -1,5 +1,14 @@
 import '../lib/actividad6.dart';
+import 'dart:io';
 
 void main() {
   print(factorial(5));
+  print(potencia(5, 3));
+  stdout.write("\nIngrese la base del triángulo: ");
+  double baseTriangulo = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Ingrese la altura del triángulo: ");
+  double altura = double.parse(stdin.readLineSync()!);
+
+  print("Área del triángulo = ${areaTriangulo(baseTriangulo, altura)}");
 }
