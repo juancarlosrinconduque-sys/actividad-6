ee7ade85ae1b0c17f708dec9725a663d15d76b60
juan <juancarlosrinconduque@gmail.com>
2026-06-10 09:34:42 -0500

    segunda version

--- lib/actividad6.dart
+++ lib/actividad6.dart
@@ -9,3 +9,15 @@ int factorial(int n) {
 }
 
 //funcion para calcular potencia
+num potencia(num base, int exponente) {
+  num resultado = 1;
+  for (int i = 0; i < exponente; i++) {
+    resultado *= base;
+  }
+  return resultado;
+}
+
+//calcular area de un triangulo
+double areaTriangulo(double b, double a) {
+  return (b * a) / 2;
+}
